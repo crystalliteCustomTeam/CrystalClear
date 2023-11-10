e94875a25864d6472a84fbfac19646c777915bd6
devsahal-crystallite <143542001+devsahal-crystallite@users.noreply.github.com>
2023-11-11 01:48:23 +0500

    pages added

--- resources/views/demo2/allbrands.blade.php
+++ resources/views/demo2/allbrands.blade.php
@@ -17,23 +17,23 @@
     <!-- End fonts -->
 
     <!-- core:css -->
-    <link rel="stylesheet" href="{{ asset('assets/vendors/core/core.css') }}../">
-    <!-- endinject -->
+  <link rel="stylesheet" href="{{ asset('assets/vendors/core/core.css') }}">
+  <!-- endinject -->
 
-    <!-- Plugin css for this page -->
-    <link rel="stylesheet" href="../assets/vendors/flatpickr/flatpickr.min.css">
-    <!-- End plugin css for this page -->
+  <!-- Plugin css for this page -->
+  <link rel="stylesheet" href="{{ asset('assets/vendors/flatpickr/flatpickr.min.css') }}">
+  <!-- End plugin css for this page -->
 
-    <!-- inject:css -->
-    <link rel="stylesheet" href="{{ asset('assets/fonts/feather-font/css/iconfont.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/vendors/flag-icon-css/css/flag-icon.min.css') }}">
-    <!-- endinject -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="{{ asset('assets/fonts/feather-font/css/iconfont.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/vendors/flag-icon-css/css/flag-icon.min.css') }}">
+  <!-- endinject -->
 
-    <!-- Layout styles -->
-    <link rel="stylesheet" href="{{ asset('assets/css/demo2/style.css') }}">
-    <!-- End layout styles -->
+  <!-- Layout styles -->
+  <link rel="stylesheet" href="{{ asset('assets/css/demo2/style.css') }}">
+  <!-- End layout styles -->
 
-    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}" />
+  <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}" />
 </head>
 
 <body>
--- resources/views/demo2/dashboard.blade.php
+++ resources/views/demo2/dashboard.blade.php
@@ -15,21 +15,21 @@
   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
   <!-- End fonts -->
 
-	<!-- core:css -->
-	<link rel="stylesheet" href="{{ asset('assets/vendors/core/core.css') }}../">
-	<!-- endinject -->
+ <!-- core:css -->
+  <link rel="stylesheet" href="{{ asset('assets/vendors/core/core.css') }}">
+  <!-- endinject -->
 
-	<!-- Plugin css for this page -->
-	<link rel="stylesheet" href="../assets/vendors/flatpickr/flatpickr.min.css">
-	<!-- End plugin css for this page -->
+  <!-- Plugin css for this page -->
+  <link rel="stylesheet" href="{{ asset('assets/vendors/flatpickr/flatpickr.min.css') }}">
+  <!-- End plugin css for this page -->
 
-	<!-- inject:css -->
-	<link rel="stylesheet" href="{{ asset('assets/fonts/feather-font/css/iconfont.css') }}">
-	<link rel="stylesheet" href="{{ asset('assets/vendors/flag-icon-css/css/flag-icon.min.css') }}">
-	<!-- endinject -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="{{ asset('assets/fonts/feather-font/css/iconfont.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/vendors/flag-icon-css/css/flag-icon.min.css') }}">
+  <!-- endinject -->
 
-  <!-- Layout styles -->  
-	<link rel="stylesheet" href="{{ asset('assets/css/demo2/style.css') }}">
+  <!-- Layout styles -->
+  <link rel="stylesheet" href="{{ asset('assets/css/demo2/style.css') }}">
   <!-- End layout styles -->
 
   <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}" />
